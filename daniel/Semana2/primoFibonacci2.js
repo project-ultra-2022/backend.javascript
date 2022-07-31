d8eb7262ab86047862c2cd3e281295e2ab9bacf0
neildan <felipeluiz152000@gmail.com>
2022-07-31 14:29:46 -0500

    Ejercicios

--- daniel/Semana2/primoFibonacci2.js
+++ daniel/Semana2/primoFibonacci2.js
@@ -1,25 +1,24 @@
 let noparar = true;
-let contador = 0;
 let primos = [];
-let sizeCoincidences = 50000;
+let sizeCoincidences = 150000;
 for (let j = 2; noparar; j++) {
     try {
         for (let i = 2; i < j; i++) {
             if (j % i === 0) throw false;
         }
         primos.push(j);
-        contador++;
-        if (contador == sizeCoincidences) noparar = false;
+        if(primos.length == sizeCoincidences) noparar = false;
     } catch(e){}
 }
 
 let num1 = 1
 let num2 = 1
 let fibonacci = [];
-
-for (let i = 0; i < sizeCoincidences; i++) {
+noparar = true;
+for (let i = 0; noparar; i++) {
     let num3 = num1 + num2
     fibonacci.push(num3)
+    if(fibonacci.length == sizeCoincidences) noparar = false
     num1 = num2
     num2 = num3
 }
