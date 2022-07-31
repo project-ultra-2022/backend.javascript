2b1a891647989ca26aba7f6bbb3756d1b05a2abf
WonderEstCode <estebanluis292005@gmail.com>
2022-07-30 22:46:33 -0500

    terminé quiz, también tarea dos: dos ejercicios

---
+++ esteban/tareas/tarea2/ejercicio2.js
@@ -0,0 +1,33 @@
+// mostrar las tablas de multiplicar desde el 2 hasta el 20
+for (let i = 2; i <= 20; i++) {
+    console.log('tabla del ' +i)
+    for (let j = 1; j <= 10; j++) {
+        console.log(i+'x'+j+'='+i*j)
+        
+    }
+    
+} 
+
+
+
+
+
+
+
+
+/* codigo por terminar, solucion mas complicada 
+let num1 = 1
+for (let i = 1; i <= 10; i++) {
+    let tabla = 2
+    let result = tabla * num1
+    console.log(tabla + 'x' + num1 + '=' +result)
+    num1++
+    if (i = 10) {
+        num1 = 1
+        tabla ++
+        if (tabla == 20) {
+            i = 11
+        }
+    }
+     
+}*/
